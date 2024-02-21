39394a8b1438397bae13980ccd3175b0905995ad
Ciro <cironidas@gmail.com>
2024-02-21 18:09:49 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -172,18 +172,7 @@ def translate(text):
 
 
 ruta_imagen_local = os.path.join("media", "logo.png")
-st.markdown(
-    """
-    <style>
-    .favicon {
-        border-radius: 50%;
-        display: block;
-        margin: 0 auto;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 st.set_page_config(page_icon=ruta_imagen_local, page_title="ExpectedFoot")
 
 st.image(ruta_imagen_local, width=400)
